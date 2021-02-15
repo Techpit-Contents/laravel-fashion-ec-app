00a7f614e3badbb0b4c35d5a294c5e0cd4451076
Hikaru <auron0330@gmail.com>
2021-02-16 01:39:03 +0900

    initial commit

---
+++ images_downloader.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir ./public/images
+for var in 1 2 3 4 5 6 7 8 9
+do
+    curl -o ./public/images/${var}.jpg https://raw.githubusercontent.com/Techpit-Contents/laravel-fashion-ec-app/chapter-5/public/images/${var}.jpg
+done
+curl -o ./public/images/top.jpg https://raw.githubusercontent.com/Techpit-Contents/laravel-fashion-ec-app/chapter-5/public/images/top.jpg
+curl -o ./public/images/top2.jpg https://raw.githubusercontent.com/Techpit-Contents/laravel-fashion-ec-app/chapter-5/public/images/top2.jpg
