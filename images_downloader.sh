a3981ec45f0270020e8de294d351edf42bb811ca
Hikaru <auron0330@gmail.com>
2021-02-17 00:09:28 +0900

    fix sh

--- images_downloader.sh
+++ images_downloader.sh
@@ -3,7 +3,7 @@
 mkdir ./public/images
 for var in 1 2 3 4 5 6 7 8 9
 do
-    curl -o ./public/images/${var}.jpg https://raw.githubusercontent.com/Techpit-Contents/laravel-fashion-ec-app/chapter-5/public/images/${var}.jpg
+    curl -o ./public/images/${var}.jpg https://raw.githubusercontent.com/Techpit-Contents/laravel-fashion-ec-app/master/public/images/${var}.jpg
 done
-curl -o ./public/images/top.jpg https://raw.githubusercontent.com/Techpit-Contents/laravel-fashion-ec-app/chapter-5/public/images/top.jpg
-curl -o ./public/images/top2.jpg https://raw.githubusercontent.com/Techpit-Contents/laravel-fashion-ec-app/chapter-5/public/images/top2.jpg
+curl -o ./public/images/top.jpg https://raw.githubusercontent.com/Techpit-Contents/laravel-fashion-ec-app/master/public/images/top.jpg
+curl -o ./public/images/top2.jpg https://raw.githubusercontent.com/Techpit-Contents/laravel-fashion-ec-app/master/public/images/top2.jpg
