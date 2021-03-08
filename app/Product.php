08258fb5100aaeaa78a5fa32681c1da5fa5ca0a2
hinoue <auron0330@gmail.com>
2021-03-08 14:02:15 +0900

    fix

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function carts()
+    {
+        return $this->belongsToMany(
+          Cart::class,
+          'line_items',
+        )->withPivot(['id', 'quantity']);
+    }
 }
