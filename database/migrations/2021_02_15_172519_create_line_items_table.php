108fb056fb906d6a6cecd664ce0bbbfce887bb15
hinoue <auron0330@gmail.com>
2021-03-08 14:04:42 +0900

    fix

--- database/migrations/2021_02_15_172519_create_line_items_table.php
+++ database/migrations/2021_02_15_172519_create_line_items_table.php
@@ -15,10 +15,17 @@ class CreateLineItemsTable extends Migration
     {
         Schema::create('line_items', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('cart_id');
-            $table->integer('product_id');
+            $table->unsignedBigInteger('cart_id');
+            $table->unsignedBigInteger('product_id');
             $table->integer('quantity');
             $table->timestamps();
+            
+            $table->foreign('cart_id')
+                ->references('id')
+                ->on('carts');
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products');
         });
     }
 
